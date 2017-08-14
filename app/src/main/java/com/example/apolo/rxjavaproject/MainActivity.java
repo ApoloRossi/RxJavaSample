39c701871018ee6db109d32db538433ec4e98239
Apolo <apolorossi@gmail.com>
2017-08-14 17:31:19 -0300

    inserindo biblioteca reativa

--- app/src/main/java/com/example/apolo/rxjavaproject/MainActivity.java
+++ app/src/main/java/com/example/apolo/rxjavaproject/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.apolo.rxjavaproject;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.reactivex.Observable;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<String> myObservable ;
+
     }
 }
